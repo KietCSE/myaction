3d8b85bbaa052eee23405f1b723083ef9038ead1
KietCSE <ptkiet170104@gmail.com>
2024-08-28 21:49:00 +0700

    create new action

---
+++ index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/exec')
+const exec = require('@actions/exec')
+
+async function run() {
+    try {
+        const username = core.getInput('username')
+        const email = core.getInput('email')
+        const token = core.getInput('token')
+    
+         // Configure git
+        await exec.exec('git', ['config', '--global', 'user.name', username]);
+        await exec.exec('git', ['config', '--global', 'user.email', email]);
+    
+        // Add changes and commit
+        await exec.exec('git', ['add', '.']);
+        await exec.exec('git', ['commit', '-m', 'New bot commit']);
+        await exec.exec('git', ['push', '-u', 'origin', 'main'], {
+            env: { GITHUB_TOKEN: token }
+        });
+    }
+    catch (err) {
+        core.setFailed(err)
+    }
+}
+
+run()
+
